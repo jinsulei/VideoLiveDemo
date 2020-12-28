50ebd6491e0e2d157808803c8bf3ddb5b74e8bab
iMusic_xc <573653911@qq.com>
2020-12-28 16:52:50 +0800

    代码

--- VideoLiveProgect/Pods/Target Support Files/FSPagerView/FSPagerView-umbrella.h
+++ VideoLiveProgect/Pods/Target Support Files/FSPagerView/FSPagerView-umbrella.h
@@ -10,7 +10,8 @@
 #endif
 #endif
 
+#import "FSPagerViewObjcCompat.h"
 
-FOUNDATION_EXPORT double SwiftyJSONVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftyJSONVersionString[];
+FOUNDATION_EXPORT double FSPagerViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char FSPagerViewVersionString[];
 
